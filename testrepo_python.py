a146c5f1f0ee5d5ea31af3adf09d725806bcb875
agnesjohansson <agnes@decuria.se>
2015-08-05 11:14:11 +0200

    #3 Skriv om programmet med dictionaries

--- testrepo_python.py
+++ testrepo_python.py
@@ -1,4 +1,7 @@
-# coding: utf-8
+robot = {"price": 900, "count": 2, "vat":1.25}
+book = {"price": 100, "count": 1, "vat":1.06}
+
+print robot["price"]*robot["count"]*robot["vat"]+book["price"]*book["count"]*book["vat"]
 
 robot_price = 900
 robot_count = 2
@@ -7,4 +10,3 @@ book_price = 100
 book_count = 1
 book_vat = 1.06
 
-print robot_price*robot_count*robot_vat+book_price*book_count*book_vat
