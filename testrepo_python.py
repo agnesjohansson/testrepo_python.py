1fd6911d227f7501e43783bde13b72ef9c62f2b3
agnesjohansson <agnes@decuria.se>
2015-08-05 10:53:39 +0200

    #2 Skriv om programmet med variabler!

--- testrepo_python.py
+++ testrepo_python.py
@@ -1,1 +1,10 @@
-print 900*2*1.25+100*1*1.06
+# coding: utf-8
+
+robot_price = 900
+robot_count = 2
+robot_vat = 1.25
+book_price = 100
+book_count = 1
+book_vat = 1.06
+
+print robot_price*robot_count*robot_vat+book_price*book_count*book_vat
