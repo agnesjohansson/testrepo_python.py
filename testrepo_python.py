9be0ea7f45568e7bc757aac331330f2e8ae8a9d2
agnesjohansson <agnes@decuria.se>
2015-08-05 11:21:40 +0200

    #4 Skriv om programmet med en klass

--- testrepo_python.py
+++ testrepo_python.py
@@ -1,7 +1,20 @@
-robot = {"price": 900, "count": 2, "vat":1.25}
-book = {"price": 100, "count": 1, "vat":1.06}
+class Product(object):
+	price = 0
+	count = 0
+	vat = 0
 
-print robot["price"]*robot["count"]*robot["vat"]+book["price"]*book["count"]*book["vat"]
+robot = Product()
+robot.price = 900
+robot.count = 2
+robot.vat	= 1.25
+
+book = Product()
+book.price = 100
+book.count = 1
+book.vat =1.06
+
+
+print robot.price*robot.count*robot.vat+book.price*book.count*book.vat
 
 robot_price = 900
 robot_count = 2
