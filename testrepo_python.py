51a82c25aea219395ecc91b11ff80eed3979e06b
agnesjohansson <agnes@decuria.se>
2015-08-05 14:18:17 +0200

    #9 Skriv om programmet med rabatt

--- testrepo_python.py
+++ testrepo_python.py
@@ -9,7 +9,11 @@ class Product(object):
 		self.vat = vat
 
 	def price_with_vat(self):
-		return self.price * self.count * self.vat
+			total = self.price * self.count * self.vat
+			if self.price > 500:
+					return 0.9*total
+			else:
+				return total
 
 robot = Product(price=900, count=2, vat=1.25)
 book =Product(price=100, count=1, vat=1.08)
